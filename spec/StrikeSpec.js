e17d19df3d4bd5fe09100bc54c0e9f28fef1edab
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-14 14:27:47 +0000

    Does not allocate the strike bonus when score is not 10

--- spec/StrikeSpec.js
+++ spec/StrikeSpec.js
@@ -6,7 +6,7 @@ describe('Strike', function() {
     strike = new Strike();
   });
 
-  describe('bonus', function() {
+  describe('bonus allocation', function() {
 
     it('is not activated by default', function() {
       expect(strike.bonusActivated).toBe(false);
@@ -15,6 +15,11 @@ describe('Strike', function() {
     it('is activated when first throw scores 10', function() {
       strike.bonusChecker(10);
       expect(strike.bonusActivated).toBe(true);
+    });
+
+    it('is not activated when first throw score is not 10', function() {
+      strike.bonusChecker(4);
+      expect(strike.bonusActivated).toBe(false);
     });
 
   });
